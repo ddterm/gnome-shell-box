ecec5ff5c2653af5c7778aaa10c4252dbbd27ceb
Aleksandr Mezin <mezin.alexander@gmail.com>
2024-09-28 12:30:31 +0300

    Fix Renovate config

--- renovate-local-dry-run.sh
+++ renovate-local-dry-run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-export LOG_LEVEL="${LOG_LEVEL:-debug}"
+export LOG_LEVEL="${LOG_LEVEL:-info}"
+export RENOVATE_REPORT_TYPE="${RENOVATE_REPORT_TYPE:-logging}"
 
 exec podman run \
   --rm -it \
   --userns=keep-id \
   -e LOG_LEVEL \
   -e GITHUB_COM_TOKEN \
+  -e RENOVATE_REPORT_TYPE \
   -v "$PWD:$PWD" \
   -w "$PWD" \
-  ghcr.io/renovatebot/renovate:slim \
+  ghcr.io/renovatebot/renovate \
   --platform=local \
   "$@"
