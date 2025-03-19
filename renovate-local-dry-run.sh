19850bfd29f44f1dc5285f49c7a5f6b88c303a4f
Aleksandr Mezin <mezin.alexander@gmail.com>
2025-03-19 17:57:24 +0200

    Fix Renovate local run script

--- renovate-local-dry-run.sh
+++ renovate-local-dry-run.sh
@@ -2,15 +2,23 @@
 
 export LOG_LEVEL="${LOG_LEVEL:-info}"
 export RENOVATE_REPORT_TYPE="${RENOVATE_REPORT_TYPE:-logging}"
+export RENOVATE_BASE_DIR="${RENOVATE_BASE_DIR:-$PWD/.renovate}"
+export RENOVATE_PLATFORM="${RENOVATE_PLATFORM:-local}"
+
+mkdir -p "$RENOVATE_BASE_DIR"
 
 exec podman run \
   --rm -it \
-  --userns=keep-id \
+  --userns=keep-id:uid=12021,gid=0 \
+  -e LOG_CONTEXT \
+  -e LOG_FILE \
+  -e LOG_FILE_LEVEL \
+  -e LOG_FORMAT \
   -e LOG_LEVEL \
   -e GITHUB_COM_TOKEN \
-  -e RENOVATE_REPORT_TYPE \
-  -v "$PWD:$PWD" \
+  -e 'RENOVATE_*' \
+  -v "$PWD:$PWD:ro" \
+  -v "$RENOVATE_BASE_DIR:$RENOVATE_BASE_DIR:U,rw" \
   -w "$PWD" \
   ghcr.io/renovatebot/renovate \
-  --platform=local \
   "$@"
